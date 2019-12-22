24f732df0adbc74ef2737774cad1acfbd6d13324
Israr <ahh.devoders@gmail.com>
2019-12-23 00:08:05 +0500

    Facebook login integrated

--- FaceBook Login Integration/ViewController.swift
+++ FaceBook Login Integration/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 class ViewController: UIViewController {
 
@@ -15,6 +17,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func loginWithFacebook(_ sender: Any){
+        let manager = LoginManager()
+        manager.logIn(permissions: [.publicProfile, .email], viewController: self) { (result) in
+            switch result{
+            case .cancelled:
+                print("User cancelled login process")
+                break
+            case .failed(let error):
+                print("Login failed with error = \(error.localizedDescription)")
+                break
+            case .success(let granted, let declined, let token):
+                print("Access token = \(AccessToken.self)")
+                
+            }
+        }
+    }
 
 }
 
